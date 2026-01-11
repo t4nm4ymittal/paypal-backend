8652c4f5d27a68e6b3cf8cc318fb3dae75f68bbf
t4nm4ymittal <2020ucp1795@mnit.ac.in>
2026-01-11 13:30:28 +0530

    MOVED PROPS TO TEMP LOC

--- reward-service/src/main/java/com/paypal/reward_service/controller/RewardController.java
+++ reward-service/src/main/java/com/paypal/reward_service/controller/RewardController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rewards")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "${cors.allowed.origins:http://localhost:3000}")
 public class RewardController {
 
     private final RewardService rewardService;
